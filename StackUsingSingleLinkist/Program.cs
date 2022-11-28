af16bdbea963745d8a1122003023eda85f373b11
DhioAWP <dhioanugerah0@gmail.com>
2022-11-28 16:23:52 +0700

    Add project files.

---
+++ StackUsingSingleLinkist/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingSingleLinkList
+{
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i, int n)
+        {
+            info = i;
+            next = n;
+        }
+    }
+}
